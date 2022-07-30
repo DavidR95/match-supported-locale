4b6a45bff27b74ad30fb05d459e307623bdafad6
David Robertson <dmichaelrobertson@gmail.com>
2022-07-30 11:23:28 +0100

    Configure Typechecking rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,19 @@ module.exports = {
     es2022: true,
   },
   extends: ['eslint:recommended', 'prettier'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: 'tsconfig.json',
+      },
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      plugins: ['@typescript-eslint'],
+    },
+  ],
 };
